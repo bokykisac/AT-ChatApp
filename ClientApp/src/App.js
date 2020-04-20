a5804c710c8213604bdffb561eda37c378c32ce9
bokykisac <dervy97@gmail.com>
2020-04-20 22:53:08 +0200

    Dodavanje front-a

---
+++ ClientApp/src/App.js
@@ -0,0 +1,233 @@
+import React from 'react';
+import './App.css';
+import axios from './axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends React.Component {
+
+  state = {
+    username: '',
+    password: '',
+    messageAll: '',
+    messageUser: '',
+    isSigningUp: false,
+    isLoggedIn: false,
+    loggedUser: ''
+  }
+
+  componentDidMount() {
+
+    // if(sessionStorage.getItem('username')){
+    //   this.setState({isLoggedIn: true, loggedUser: sessionStorage.getItem('username')})
+    // }
+
+    let socket;
+    const host = "ws://localhost:8080/WAR2020/ws";
+
+    try {
+      socket = new WebSocket(host);
+      console.log('connect: Socket Status: ' + socket.readyState);
+
+      socket.onopen = function () {
+        console.log('onopen: Socket Status: ' + socket.readyState + ' (open)');
+      }
+
+      socket.onmessage = function (msg) {
+        console.log('onmessage: Received: ' + msg.data);
+      }
+
+      socket.onclose = function () {
+        socket = null;
+      }
+
+    } catch (exception) {
+      console.log('Error' + exception);
+    }
+  }
+
+  onClick = () => {
+    axios.post("rest/chat/post/asdfasd", {})
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  onSwitchFormHandler = () => {
+    this.setState({ isSigningUp: !this.state.isSigningUp });
+  }
+
+  onRegisterHandler = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios.post("rest/chat/users/register", user)
+      .then(res => {
+        alert('Uspesna registracija')
+        this.setState({isSigningUp: false})
+      })
+      .catch(err => alert("Greska pri registraciji/username vec postoji"));
+  }
+
+  onLoginHandler = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios.post("rest/chat/users/login", user)
+      .then(res => {
+        this.setState({ isLoggedIn: true, loggedUser: user.username });
+        // sessionStorage.setItem('username', user.username);
+      })
+      .catch(err => console.log(err));
+  }
+
+  onLogoutHandler = () => {
+    axios.delete("rest/chat/users/loggedIn/" + this.state.loggedUser)
+      .then(res => {
+        console.log(res);
+        this.setState({isLoggedIn: false, loggedUser: ''})
+      })
+      .catch(err => console.log(err))
+  }
+
+  onGetLoggedUsersHandler = () => {
+    axios.get("rest/chat/users/loggedIn")
+      .then(res => alert("Success, proveriti consolu u WildFly"))
+      .catch(err => console.log(err));
+  }
+
+  onGetRegisteredUsersHandler = () => {
+    axios.get("rest/chat/users/registered")
+      .then(res => alert("Success, proveriti consolu u WildFly"))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+
+    let form = (
+      <div className="row">
+        <form style={{ margin: 'auto' }}>
+          {this.state.isSigningUp
+            ? <h5>Register new user</h5>
+            : <h5>Login</h5>}
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              className="form-control"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </div>
+
+          <div>
+            {this.state.isSigningUp
+              ? <button className="btn btn-primary" type="button" style={{ marginRight: '10px' }} onClick={(e) => this.onRegisterHandler(e)}>Register</button>
+              : <button className="btn btn-primary" type="submit" style={{ marginRight: '10px' }} onClick={e => this.onLoginHandler(e)}>Login</button>}
+
+            {this.state.isSigningUp
+              ? <button className="btn btn-primary" type="button" onClick={this.onSwitchFormHandler} style={{ margin: '10px 0px' }}>Switch to Login</button>
+              : <button className="btn btn-primary" type="button" onClick={this.onSwitchFormHandler} style={{ margin: '10px 0px' }}>Switch to Register</button>}
+          </div>
+        </form>
+      </div>
+    );
+
+    if (this.state.isLoggedIn) {
+      form = <div className="row">
+        <h5>Logovani ste kao <strong>{this.state.loggedUser}</strong></h5>
+      </div>;
+    }
+
+    return (
+      <div className="container">
+        <div className="row">
+          <h1 style={{ margin: 'auto' }}>Chat APP</h1>
+        </div>
+
+        <hr />
+
+        {form}
+
+        <hr />
+
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-primary" type="button" onClick={this.onGetLoggedUsersHandler}>Get all logged users</button>
+          </div>
+          <div className="col">
+            <button className="btn btn-primary" type="button" onClick={this.onGetRegisteredUsersHandler}>Get all registered users</button>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <label htmlFor="messageAll">Send message to all users</label>
+            </div>
+            <div className="row">
+              <textarea name="messageAll" id="messageAll" cols="30" rows="3"></textarea>
+            </div>
+            <div className="row">
+              <button className="btn btn-primary" style={{ margin: '10px 0px' }}>Send</button>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="row">
+              <label htmlFor="selectUser">Send message to user:</label>
+              <select name="selectUser" id="selectUser" style={{ margin: '0px 10px 10px 10px' }}>
+                <option>Test</option>
+              </select>
+            </div>
+
+            <div className="row">
+              <textarea name="messageUser" id="messageUser" cols="30" rows="3"></textarea>
+            </div>
+
+            <div className="row">
+              <button className="btn btn-primary" style={{ margin: '10px 0px' }}>Send</button>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col">
+            {this.state.isLoggedIn ? <button className="btn btn-primary">Show all my messages</button> : null}
+          </div>
+
+          <div className="col">
+            {this.state.isLoggedIn ? <button className="btn btn-danger" type="button" onClick={this.onLogoutHandler}>Logout</button> : null}
+          </div>
+        </div>
+
+      </div >
+    );
+  }
+
+}
+
+export default App;
